72f63c3ad3d8c8a6ab1c4eeb4e2f9a46f8bc66bb
Iraon720 <iraon3493@gmail.com>
2023-02-27 18:21:55 -0500

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
-import Child from "./Components/Child";
-
-
+import Child from './Components/Child';
+import sedans from './images/icon-sedans.svg';
+import SUVs from './images/icon-suvs.svg';
+import Luxury from './images/icon-luxury.svg';
 const option = {
- sedans: {
+  sedans: {
     id: 'SED',
-    src: '',
+    src: sedans,
     headline: 'sedans',
     description:
       'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
     button: 'Learn more',
-    
   },
- SUVs: {
+  SUVs: {
     id: 'SUVs',
-    src: '',
+    src: SUVs,
     headline: 'SUVs',
     description:
       'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
     button: 'Learn more',
   },
- Luxury: {
-    
+  Luxury: {
     id: 'LUX',
-    src: '',
+    src: Luxury,
     headline: 'Luxury',
     description:
       'Indulge yourself in our most luxurious vehicles, featuring top-of-the-line amenities, technology, and performance.',
@@ -32,43 +31,50 @@ const option = {
   },
 };
 
-
-
 function App() {
   return (
-    <section className='house'>
-      <section className='room1'>
-      <Child
-        key={option.sedans.id}
-        headline={option.sedans.headline.toUpperCase()}
-        description={option.sedans.description}
-        icon={}
-        cardColor={option.sedans.cardColor}
-        buttonTextColor={option.sedans.buttonTextColor}
-        btnText={option.sedans.button}
-      />
+    <section className='house' >
+      <section
+        className='room1'
+        style={{ backgroundColor: 'hsl(31, 77%, 52%)' ,borderTopLeftRadius:'9px', borderBottomLeftRadius:'9px',height:'500px', width:'307px'}}
+      >
+        <Child
+          key={option.sedans.id}
+          headline={option.sedans.headline.toUpperCase()}
+          description={option.sedans.description}
+          icon={option.sedans.src}
+          cardColor={option.sedans.cardColor}
+          buttonTextColor={option.sedans.buttonTextColor}
+          btnText={option.sedans.button}
+        />
       </section>
-      <section className='room2'>
-      <Child
-        key={option.SUVs.id}
-        headline={option.SUVs.headline.toUpperCase()}
-       description={option.sedans.description}
-        icon={}
-        cardColor={option.SUVs.cardColor}
-        buttonTextColor={option.SUVs.buttonTextColor}
-        btnText={option.SUVs.button}
-      />
+      <section
+        className='room2'
+        style={{ backgroundColor: 'hsl(184, 100%, 22%)',borderTopLeftRadius:'9px', borderBottomLeftRadius:'9px', height:'500px', width:'307px'}}
+      >
+        <Child
+          key={option.SUVs.id}
+          headline={option.SUVs.headline.toUpperCase()}
+          description={option.sedans.description}
+          icon={option.SUVs.src}
+          cardColor={option.SUVs.cardColor}
+          buttonTextColor={option.SUVs.buttonTextColor}
+          btnText={option.SUVs.button}
+        />
       </section>
-      <section className='room3'>
-      <Child
-        key={option.Luxury.id}
-        headline={option.Luxury.headline.toUpperCase()}
-        description={option.Luxury.description}
-        icon={}
-        cardColor={option.Luxury.cardColor}
-        buttonTextColor={option.Luxury.buttonTextColor}
-        btnText={option.Luxury.button}
-      />
+      <section
+        className='room3'
+        style={{ backgroundColor: 'hsl(179, 100%, 13%)', borderBottomRightRadius:'9px', borderTopRightRadius:'9px', height:'500px', width:'307px'}}
+      >
+        <Child
+          key={option.Luxury.id}
+          headline={option.Luxury.headline.toUpperCase()}
+          description={option.Luxury.description}
+          icon={option.Luxury.src}
+          cardColor={option.Luxury.cardColor}
+          buttonTextColor={option.Luxury.buttonTextColor}
+          btnText={option.Luxury.button}
+        />
       </section>
     </section>
   );
--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,29 +1,21 @@
 import React from 'react'
 import  './Child.css';
 
+//for naming conventions I should be using "Props" but instead im using options.
 
-
-
-
-
-const choices = (props)=> {
+const Child = (options)=> {
   return (
     <div className='childC'>
-      <img src={props.icon} alt='' />
-      <h2>{props.headline}</h2>
-      <p>{props.description}</p>
-      <button>{props.btnText}</button>
+      <img
+        src={options.icon}
+        alt=''
+        style={{ height: '40px', width: '60px', marginTop:'27px', marginLeft:'40px' }}
+      />
+      <h2 style={{color:'white'}}>{options.headline}</h2>
+      <p style={{width:'200px'}}>{options.description}</p>
+      <button>{options.btnText}</button>
     </div>
   );
-}
+};
 
 export default Child;
-//  {
-//    choices.map((preview) => (
-//      <div key={preview.id}>
-//        <h2>{preview.headline}</h2>
-//        <p style={{ fontSize: '16px' }}>{preview.description}</p>
-//        <button>{preview.button}</button>
-//      </div>
-//    ));
-//  }
