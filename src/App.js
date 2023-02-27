312ee5c3e18c50db2c4f0629e936c63d97096896
Iraon720 <iraon3493@gmail.com>
2023-02-26 22:46:26 -0500

    Changed from array of obj to obj, moved to parent

--- src/App.js
+++ src/App.js
@@ -3,14 +3,74 @@ import './App.css';
 import Child from "./Components/Child";
 
 
+const option = {
+ sedans: {
+    id: 'SED',
+    src: '',
+    headline: 'sedans',
+    description:
+      'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+    button: 'Learn more',
+    
+  },
+ SUVs: {
+    id: 'SUVs',
+    src: '',
+    headline: 'SUVs',
+    description:
+      'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+    button: 'Learn more',
+  },
+ Luxury: {
+    
+    id: 'LUX',
+    src: '',
+    headline: 'Luxury',
+    description:
+      'Indulge yourself in our most luxurious vehicles, featuring top-of-the-line amenities, technology, and performance.',
+    button: 'Learn more',
+  },
+};
+
 
 
 function App() {
   return (
-    <div className="house" style={{ fontFamily: 'Lexend Deca, sans-serif', backgroundColor:'yellow'}}>
-     <Child/>
-
-    </div>
+    <section className='house'>
+      <section className='room1'>
+      <Child
+        key={option.sedans.id}
+        headline={option.sedans.headline.toUpperCase()}
+        description={option.sedans.description}
+        icon={}
+        cardColor={option.sedans.cardColor}
+        buttonTextColor={option.sedans.buttonTextColor}
+        btnText={option.sedans.button}
+      />
+      </section>
+      <section className='room2'>
+      <Child
+        key={option.SUVs.id}
+        headline={option.SUVs.headline.toUpperCase()}
+       description={option.sedans.description}
+        icon={}
+        cardColor={option.SUVs.cardColor}
+        buttonTextColor={option.SUVs.buttonTextColor}
+        btnText={option.SUVs.button}
+      />
+      </section>
+      <section className='room3'>
+      <Child
+        key={option.Luxury.id}
+        headline={option.Luxury.headline.toUpperCase()}
+        description={option.Luxury.description}
+        icon={}
+        cardColor={option.Luxury.cardColor}
+        buttonTextColor={option.Luxury.buttonTextColor}
+        btnText={option.Luxury.button}
+      />
+      </section>
+    </section>
   );
 }
 
--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,49 +1,29 @@
 import React from 'react'
-
-const choices =  [
-  {
-    id: 'SED',
-    src: '',
-    headline: 'sedans',
-    description:
-      'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-    button: 'Learn more',
-    cardColor: { backgroundColor: 'var(--brignt-orange)' },
-    buttonTextColor: { color: 'blue' }
-  },
-  {
-   
-    id: 'SUVs',
-     src:'',
-    headline: 'SUVs',
-    description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-      button: 'Learn more',
-  },
-  {
-    src:'',
-    id: 'LUX',
-    headline: 'Luxury',
-    description: 'Indulge yourself in our most luxurious vehicles, featuring top-of-the-line amenities, technology, and performance.',
-      button: 'Learn more',
-  }
-];
+import  './Child.css';
 
 
 
 
-  function Child() {
+
+
+const choices = (props)=> {
   return (
-    <div>
-      {choices.map(preview => (
-        <div key={preview.id}>
-          <h2>{preview.headline.toUpperCase}</h2>
-          <p style={{fontSize:'16px', }}>{preview.description}</p>
-          <button>{preview.button}</button>
-        </div>
-      ))}
+    <div className='childC'>
+      <img src={props.icon} alt='' />
+      <h2>{props.headline}</h2>
+      <p>{props.description}</p>
+      <button>{props.btnText}</button>
     </div>
   );
 }
 
-
 export default Child;
+//  {
+//    choices.map((preview) => (
+//      <div key={preview.id}>
+//        <h2>{preview.headline}</h2>
+//        <p style={{ fontSize: '16px' }}>{preview.description}</p>
+//        <button>{preview.button}</button>
+//      </div>
+//    ));
+//  }
